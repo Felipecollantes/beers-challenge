d8a10b3e34c86dbbc9f5193a93055d0ede9a1061
FelipeCollantes <felipe.pcollantes@gmail.com>
2023-09-14 19:16:02 +0200

    Add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+import { ListBeersModule } from './views/list-beers/list-beers.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, ListBeersModule],
   providers: [],
   bootstrap: [AppComponent],
 })
